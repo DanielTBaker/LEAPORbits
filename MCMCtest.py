864032f5dd66d78a293e2466605d07ce0a36847e
Daniel Baker <danieltb@telus.net>
2019-09-04 10:19:57 -0400

    Fitting Tests

---
+++ MCMCtest.py
@@ -0,0 +1,72 @@
+import orbfits
+import numpy as np
+import os
+from psrqpy import QueryATNF
+from astropy.time import Time
+import astropy.units as u
+import astropy.constants as c
+from multiprocessing import Pool
+
+os.environ["OMP_NUM_THREADS"] = "1"
+
+
+names=list(f[6:-4] for f in os.listdir('./binarytimestamps'))
+names_cat=list(names)
+names_cat[0]='J1939+2134'
+names_cat[6]=names[6]+'+2551'
+names.remove(names[6])
+names_cat.remove(names_cat[6])
+query=QueryATNF(psrs=names_cat)
+psrs = query.get_pulsars()
+
+Test_source = 'J1643-1224'
+srce=SkyCoord.from_name('PSR %s' %Test_source)
+
+PSR = psrs[Test_source]
+times_str = np.load('./binarytimestamps/times_%s.npy' %
+                    Test_source).astype(str)
+times = Time(times_str)
+
+##Knowns
+dp = PSR.DIST_DM1 * u.kpc
+Om_peri = PSR.OM * u.deg
+Om_peri_dot = PSR.OMDOT * u.deg / u.year
+A1 = PSR.A1 * u.s * const.c
+Ecc = PSR.ECC
+Pb = PSR.PB * u.day
+if str(PSR.T0) == '--':
+    TASC = Time(PSR.TASC, format='mjd')
+    T0 = Time(brentq(T0_find,
+                     TASC.mjd, (TASC + Pb).mjd,
+                     args=(TASC, -Om_peri, Pb, Ecc)),
+              format='mjd')
+else:
+    T0 = Time(PSR.T0, format='mjd')
+pm_ra = (PSR.PMRA * u.mas / u.year).to_value(u.rad / u.s) / u.s
+pm_dec = (PSR.PMDec * u.mas / u.year).to_value(u.rad / u.s) / u.s
+f0 = 1 * u.GHz
+
+##Unknowns
+Om_orb = 38.2 * u.deg
+Om_scr = 0 * u.deg
+inc = 20 * u.deg
+ds = dp / 2
+
+a = np.abs(A1 / np.sin(inc))
+eta_data = eta_orb(srce,times, a, T0, Pb, Om_peri_dot, Om_peri, Om_orb, Om_scr, inc,
+                   dp, ds, f0, pm_ra, pm_dec)
+
+sigma = eta_data / 10
+
+eta_noisy = eta_data + np.random.normal(0, 1, eta_data.shape[0])*sigma
+
+lwrs=np.array([0,0,0,0])
+uprs=np.array([360,360,360,dp.to_value(u.kpc)])
+
+pool=Pool()
+
+ndim, nwalkers = 4, 40
+pos = [np.random.uniform(lwrs,uprs) for i in range(nwalkers)]
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=(eta_noisy,sigma,srce,times,T0, Pb, Om_peri_dot, Om_peri,dp,f0,pm_ra,pm_dec, A1,lwrs,uprs),pool=pool)
+
+sampler.run_mcmc(pos, 1000, progress=True)
---
+++ orbfits.py
@@ -0,0 +1,114 @@
+from astropy.coordinates import solar_system_ephemeris, EarthLocation, SkyCoord
+from astropy.coordinates import get_body_barycentric_posvel
+from astropy.coordinates import SphericalRepresentation
+from astropy.time import Time
+import astropy.units as u
+import numpy as np
+import astropy.constants as const
+
+def E_proj_vel(srce,T):
+    srce_cart=srce.transform_to('barycentrictrueecliptic').represent_as('cartesian')
+    earth_posvel=get_body_barycentric_posvel('earth', T)
+
+    srce_dra=SkyCoord(ra=srce.ra+1e-7*u.deg,dec=srce.dec)
+    srce_dra_cart=srce_dra.transform_to('barycentrictrueecliptic').represent_as('cartesian')
+    unit_dra=srce_dra_cart-srce_cart
+    unit_dra=unit_dra/np.sqrt(unit_dra.x**2+unit_dra.y**2+unit_dra.z**2)
+
+    E_V_ra=unit_dra.dot(earth_posvel[1]).to(u.km/u.s)
+
+    srce_ddec=SkyCoord(ra=srce.ra,dec=srce.dec+1e-7*u.deg)
+    srce_ddec_cart=srce_ddec.transform_to('barycentrictrueecliptic').represent_as('cartesian')
+    unit_ddec=srce_ddec_cart-srce_cart
+    unit_ddec=unit_ddec/np.sqrt(unit_ddec.x**2+unit_ddec.y**2+unit_ddec.z**2)
+
+    E_V_dec=unit_ddec.dot(earth_posvel[1]).to(u.km/u.s)
+    return(E_V_ra,E_V_dec)
+
+def orb_proj_vel(time, a, T0, Pb, Om_peri_dot, Om_peri, Om_orb, Om_scr,
+                       inc):
+    Om_peri_cur = Om_peri + Om_peri_dot * (time - T0)
+    times = time + np.linspace(0, Pb / 1e5, 2)
+    M = ((times - T0) * (2 * np.pi * u.rad / Pb - Om_peri_dot)).to_value(u.rad)
+    TA = M + (2 * Ecc -
+              (Ecc**3) / 4) * np.sin(M) + (5 / 4) * (Ecc**2) * np.sin(
+                  2 * M) + (13 / 12) * (Ecc**3) * np.sin(3 * M)
+    R = a * (1 - Ecc**2) / (1 + Ecc * np.cos(TA))
+    x = R * np.cos(TA)
+    y = R * np.sin(TA)
+    V_x = (np.diff(x)[0] / np.diff(times)[0]).to(u.km / u.s)
+    V_y = (np.diff(y)[0] / np.diff(times)[0]).to(u.km / u.s)
+    V_x2 = V_x * np.cos(Om_peri_cur) - V_y * np.sin(Om_peri_cur)
+    V_y2 = V_x * np.sin(Om_peri_cur) + V_y * np.cos(Om_peri_cur)
+    V_y3 = V_y2 * np.cos(inc)
+    V_ra = V_x2*np.cos(Om_orb) - V_y3*np.sin(Om_orb)
+    V_dec = V_x2*np.sin(Om_orb) + V_y3*np.cos(Om_orb)
+    return (V_ra, V_dec)
+
+def eta_orb(srce,times, a, T0, Pb, Om_peri_dot, Om_peri, Om_orb, Om_scr,inc,dp,ds,f0,pm_ra,pm_dec):
+    dps = dp - ds
+    Orb_ra=np.zeros(times.shape[0])*u.km/u.s
+    Orb_dec=np.zeros(times.shape[0])*u.km/u.s
+    for i in range(times.shape[0]):
+        Orb_ra[i],Orb_dec[i]= orb_proj_vel(times[i], a, T0, Pb, Om_peri_dot, Om_peri, Om_orb, Om_scr,
+        inc)
+    V_P_ra=pm_ra*dp+Orb_ra
+    V_P_dec=pm_dec*dp+Orb_dec
+    V_E_ra,V_E_dec=E_proj_vel(srce,times)
+    V_E_par = V_E_ra*np.cos(Om_scr) + V_E_dec*np.sin(Om_scr)
+    V_P_par = V_P_ra*np.cos(Om_scr) + V_P_dec*np.sin(Om_scr)
+    Veff = V_E_par + (ds / dp) * V_P_par
+
+    eta = .5 * (ds * dp / dps) * (const.c / ((Veff)**2)) / f0**2
+    return(eta.to(u.ms/u.mHz**2))
+
+def eta_sol(times,Om_scr,dp,ds,f0,pm_ra,pm_dec):
+    dps = dp - ds
+    V_P_ra=pm_ra*dp
+    V_P_dec=pm_dec*dp
+    V_E_ra,V_E_dec=E_proj_vel(SkyCoord.from_name('PSR %s' %names_cat[srce_idx]),times)
+    V_E_par = V_E_ra*np.cos(Om_scr) + V_E_dec*np.sin(Om_scr)
+    V_P_par = V_P_ra*np.cos(Om_scr) + V_P_dec*np.sin(Om_scr)
+    Veff = V_E_par + (ds / dp) * V_P_par
+
+    eta = .5 * (ds * dp / dps) * (const.c / ((Veff)**2)) / f0**2
+    return(eta.to(u.ms/u.mHz**2))
+
+def T0_find(T0,TA,nu0,Pb,Ecc):
+    M=2*np.pi*u.rad*(TA-Time(T0,format='mjd'))/Pb
+    nu=M.value+(2*Ecc-(Ecc/2)**2)*np.sin(M)+(5/4)*(Ecc**2)*np.sin(2*M)+(13/12)*(Ecc**3)*np.sin(3*M)
+    return(nu-nu0.to_value(u.rad))
+
+def solver(X,data,sigma,times,T0, Pb, Om_peri_dot, Om_peri,dp,f0,pm_ra,pm_dec, A1):
+    Om_orb,Om_scr,inc,ds = X
+    Om_orb*=u.deg
+    Om_scr*=u.deg
+    inc*=u.deg
+    ds*=u.kpc
+    a=np.abs(A1/np.sin(inc))
+    eta_model=eta_orb(times, a, T0, Pb, Om_peri_dot, Om_peri, Om_orb, Om_scr,inc,dp,ds,f0,pm_ra,pm_dec)
+    val=np.sum(((eta_model-data)/sigma)**2)
+    print(X,' : ', val)
+    return(val)
+    
+def lnprior(theta,lwrs,uprs):
+    if (theta-lwrs).min()<0:
+        return(-np.inf)
+    if (theta-uprs).max()>0:
+        return(-np.inf)
+    return(0.0)
+   
+def lnprob(theta,data,sigma,srce,times,T0, Pb, Om_peri_dot, Om_peri,dp,f0,pm_ra,pm_dec, A1,lwrs,uprs):
+    Om_orb,Om_scr,inc,ds = theta
+    Om_orb*=u.deg
+    Om_scr*=u.deg
+    inc*=u.deg
+    ds*=u.kpc
+    lp=lnprior(theta,lwrs,uprs)
+    if not np.isfinite(lp):
+        return -np.inf
+    inv_sigma2 = 1.0/sigma**2
+    model=eta_orb(srce,times, a, T0, Pb, Om_peri_dot, Om_peri, Om_orb, Om_scr,inc,dp,ds,f0,pm_ra,pm_dec)
+    
+    lp2 = -0.5*(np.sum((data-model).value**2*inv_sigma2.value - np.log(inv_sigma2.value)))
+    return(lp+lp2)
