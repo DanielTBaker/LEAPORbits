342ecfec29c33bef2fc370c61dbf9ced8b26392c
Daniel Baker <danieltb@telus.net>
2019-09-06 03:43:39 -0400

    Multiple Sims

--- MCMCtest.py
+++ MCMCtest.py
@@ -21,6 +21,7 @@ parser.add_argument('-i',type=float,default=45,help='Inclination')
 parser.add_argument('-s',type=float,default=.5,help='Fractional Screen Distance')
 parser.add_argument('-ml',action='store_true',default= False, help='Maximum Likelihood')
 parser.add_argument('-mm',type=str,default= 'L-BFGS-B', help='Maximum Likelihood Method')
+parser.add_argument('-np',type=int,default= 0, help='Number of Random Parameters')
 
 
 args=parser.parse_args()
@@ -149,4 +150,88 @@ plt.ylabel(r'$\nu$ ($ms/mHz^{2}$)')
 plt.title('Fit Results')
 plt.savefig('FIT.png')
 
-np.save('samples.npy',sampler.chain)
+np.savez('samples.npz',samps=sampler.chain,reals=reals)
+
+for i in range(args.np):
+        ##Unknowns
+    Om_orb = np.mod(args.oo,360) * u.deg
+    Om_scr = (np.mod(args.os+90,180)-90) * u.deg
+    inc = np.mod(args.i,90) * u.deg
+    ds = dp *args.s
+
+    print('Simulate Data')
+    a = np.abs(A1 / np.sin(inc))
+    eta_data = orbfits.eta_orb(srce,times,Ecc, a, T0, Pb, Om_peri_dot, Om_peri, Om_orb, Om_scr, inc,
+                    dp, ds, f0, pm_ra, pm_dec)
+
+    sigma = eta_data / 10
+
+    eta_noisy = eta_data + np.random.normal(0, 1, eta_data.shape[0])*sigma
+
+    lwrs=np.array([0,-90,0,0])
+    uprs=np.array([360,90,90,dp.to_value(u.kpc)])
+
+    if args.ml:
+        nll = lambda *args: -orbfits.lnprob(*args)
+        print(nll((lwrs+uprs)/2,eta_noisy,sigma,srce,times,Ecc,T0, Pb, Om_peri_dot, Om_peri,dp,f0,pm_ra,pm_dec, A1,lwrs,uprs))
+        result = minimize(nll, (lwrs+uprs)/2, args=(eta_noisy,sigma,srce,times,Ecc,T0, Pb, Om_peri_dot, Om_peri,dp,f0,pm_ra,pm_dec, A1,lwrs,uprs),bounds=[(lwrs[i],uprs[i]) for i in range(ndim)],method=args.mm)
+        print(result.x)
+        pos = [result.x + 1e-2*np.random.randn(ndim)*result.x for i in range(nwalkers)]
+    else:
+        pos = [np.random.uniform(lwrs,uprs) for i in range(nwalkers)]
+
+
+    print('Start Walking')
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, orbfits.lnprob, args=(eta_noisy,sigma,srce,times,Ecc,T0, Pb, Om_peri_dot, Om_peri,dp,f0,pm_ra,pm_dec, A1,lwrs,uprs),threads=20)
+
+    sampler.run_mcmc(pos, args.ns)
+    print('Walk Complete')
+
+    samples = sampler.chain[:, min((1000,args.ns//2)):, :].reshape((-1, ndim))
+
+    lwrs=samples.mean(0)-np.array([180,-90,-45,0])
+    uprs=samples.mean(0)+np.array([360,180,90,0])
+    lwrs[-1]=0
+    uprs[-1]=dp.to_value(u.kpc)
+    pos=sampler.chain[:,-1,:]
+    print('Start Walk 2')
+    sampler.run_mcmc(pos, args.ns)
+    samples = sampler.chain[:, min((1000,args.ns//2)):, :].reshape((-1, ndim))
+    print('Walk Complete')
+
+
+    para_names=np.array([r'$\Omega_{orb}$',r'$\Omega_{scr}$',r'$i$',r'$D_s$'])
+    para_names_file=np.array(['OmOrb','OmScr','i','Ds'])
+    reals=np.array([Om_orb.value,Om_scr.value,inc.value,ds.value])
+
+    fig = corner.corner(samples, labels=para_names,
+                        truths=reals)
+    fig.savefig("Corner_%s.png" %(i+1))
+
+    for k in range(4):
+        plt.figure()
+        for i in range(nwalkers):
+            plt.plot(sampler.chain[i,:,k])
+        plt.title(para_names[k])
+        plt.axhline(reals[k],color='k',linewidth=2)
+        plt.savefig('%s_walk_%s.png' %(para_names_file[k],i+1))
+
+    times_curve=Time(np.linspace(times.min().mjd,times.max().mjd,10000),format='mjd')
+
+    eta_fit = orbfits.eta_orb(srce,times_curve,Ecc, a, T0, Pb, Om_peri_dot, Om_peri, samples[:,0].mean()*u.deg, samples[:,1].mean()*u.deg, samples[:,2].mean()*u.deg,
+                    dp, samples[:,3].mean()*u.kpc, f0, pm_ra, pm_dec)
+    eta_real = orbfits.eta_orb(srce,times_curve,Ecc, a, T0, Pb, Om_peri_dot, Om_peri, Om_orb, Om_scr, inc,
+                    dp, ds, f0, pm_ra, pm_dec)
+
+    plt.figure()
+    plt.plot_date(times.plot_date,eta_noisy,label='Data')
+    plt.plot_date(times_curve.plot_date,eta_fit,'-',label='Fit')
+    plt.plot_date(times_curve.plot_date,eta_real,'-',label='Real')
+    plt.legend(loc=0)
+    plt.yscale('log')
+    plt.xlabel('Date')
+    plt.ylabel(r'$\nu$ ($ms/mHz^{2}$)')
+    plt.title('Fit Results')
+    plt.savefig('FIT.png')
+
+    np.savez('samples_%s.npz' %(i+1),samps=sampler.chain,reals=reals)
