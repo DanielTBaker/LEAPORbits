1d83b911547c71e4f0f3d54e571a32e3b28d0d89
Daniel Baker <danieltb@telus.net>
2019-09-18 00:44:21 -0400

    Get Parameters from Data Directory

--- PTData.py
+++ PTData.py
@@ -58,9 +58,9 @@ if not os.path.isfile('%s/%s_params.npy' %(dirname,Source)):
     pm_ra = (PSR.PMRA * u.mas / u.year).to_value(u.rad / u.s) / u.s
     pm_dec = (PSR.PMDec * u.mas / u.year).to_value(u.rad / u.s) / u.s
     srce=SkyCoord.from_name('PSR %s' %Source)
-    np.save('%s_params.npy' %Source,np.array([dp.value, Om_peri.value, Om_peri_dot.value, A1.value, Ecc, Pb.value, T0.mjd,pm_ra.value,pm_dec.value,srce.ra.to_value(u.deg),srce.dec.to_value(u.deg)]))
+    np.save('%s/%s_params.npy' %(dirname,Source),np.array([dp.value, Om_peri.value, Om_peri_dot.value, A1.value, Ecc, Pb.value, T0.mjd,pm_ra.value,pm_dec.value,srce.ra.to_value(u.deg),srce.dec.to_value(u.deg)]))
 else:
-    dp, Om_peri, Om_peri_dot, A1, Ecc, Pb, T0,pm_ra,pm_dec,ra,dec = np.load('%s_params.npy' %Source)
+    dp, Om_peri, Om_peri_dot, A1, Ecc, Pb, T0,pm_ra,pm_dec,ra,dec = np.load'%s/%s_params.npy' %(dirname,Source))
     dp*=u.kpc
     Om_peri*=u.deg
     Om_peri_dot*=u.deg/u.year
