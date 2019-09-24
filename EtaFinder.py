b6c97f58fa6f7124cdd986f47a798730e4013841
Daniel Baker <danieltb@telus.net>
2019-09-24 07:58:30 -0400

    No mean division

--- EtaFinder.py
+++ EtaFinder.py
@@ -102,9 +102,6 @@ with PdfPages('%s/%s_etas.pdf' %(dirname_save,srce)) as pdf:
             else:
                 rbin=args.rbin
             dspec=data['I']
-            mu=dspec.mean(0)
-            dspec/=mu
-            dspec[:,mu==0]=0
             eta_est[i],eta_low[i],eta_high[i]=datareader.eta_from_data(dspec,data['freq'],data['time'],rbin=rbin,rbd=args.rbd,xlim=args.fP,ylim=args.tP,tau_lim=args.tH*u.us,fd_lim=args.fH*u.mHz,srce=srce,prof=data['prof'],template=data['template'],Nr=args.Nr)
             pdf.savefig()
         except:
